ddf8561643d0c9d7a84cc05d3754b17527d7a6a5
asunwei <993525033@qq.com>
2016-11-18 21:32:05 +0800

    Update app.component.ts

--- customform/app/app.component.ts
+++ customform/app/app.component.ts
@@ -1,1 +1,7 @@
-import { Component } from '@angular/core';@Component({    selector: 'my-app',    template: '<hero-form></hero-form>'})export class AppComponent { }
+import { Component } from '@angular/core';
+@Component(
+        { selector: 'my-app',    
+          template: '<hero-form></hero-form>'
+        })
+ 
+ export class AppComponent { }
